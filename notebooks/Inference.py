d1bac7c54c28d1334c9ec2fcbfc8efdc132a3ad8
somnathbanerjee2032 <somnathbanerjee2032>
2024-06-25 10:38:55 +0000

    change

--- notebooks/Inference.py
+++ notebooks/Inference.py
@@ -5,6 +5,11 @@
 
 # COMMAND ----------
 
+# MAGIC %pip install numpy==1.19.1
+# MAGIC %pip install pandas==1.0.5
+
+# COMMAND ----------
+
 from sparkmeasure import StageMetrics, TaskMetrics
 
 taskmetrics = TaskMetrics(spark)
